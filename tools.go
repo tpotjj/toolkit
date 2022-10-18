d5e7172b8a26321089b51d30bd61f42de9baa639
Joris Jansen <j.jansen@ispnext.com>
2022-10-18 05:52:08 +0200

    Added ErrorJONS, ReadJSON & WriteJSON

--- tools.go
+++ tools.go
@@ -2,6 +2,7 @@ package toolkit
 
 import (
 	"crypto/rand"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -19,8 +20,10 @@ const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 // Any variable of this type will have access to all the methods
 // with the receiver with the reciever *Tools
 type Tools struct {
-	MaxFileSize      int
-	AllowedFileTypes []string
+	MaxFileSize        int
+	AllowedFileTypes   []string
+	MaxJSONSize        int
+	AllowUnknownFields bool
 }
 
 // RandomString returns a string of random characters of length n
@@ -49,12 +52,12 @@ func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool)
 		renameFile = rename[0]
 	}
 
-    files, err := t.UploadFiles(r, uploadDir, renameFile)
-    if err != nil {
-        return nil, err
-    }
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+	if err != nil {
+		return nil, err
+	}
 
-    return files[0], nil
+	return files[0], nil
 }
 
 func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
@@ -69,10 +72,10 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 		t.MaxFileSize = 1024 * 1024 * 1024
 	}
 
-    err := t.CreateDirIfNotExists(uploadDir)
-    if err != nil {
-        return nil, err
-    }
+	err := t.CreateDirIfNotExists(uploadDir)
+	if err != nil {
+		return nil, err
+	}
 
 	err = r.ParseMultipartForm(int64(t.MaxFileSize))
 	if err != nil {
@@ -80,7 +83,9 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 	}
 
 	for _, fHeaders := range r.MultipartForm.File {
-		for _, hdr := range fHeaders { uploadedFiles, err = func(uploadedFiles []*UploadedFile) ([]*UploadedFile, error) { var uploadedFile UploadedFile
+		for _, hdr := range fHeaders {
+			uploadedFiles, err = func(uploadedFiles []*UploadedFile) ([]*UploadedFile, error) {
+				var uploadedFile UploadedFile
 				infile, err := hdr.Open()
 				if err != nil {
 					return nil, err
@@ -119,30 +124,30 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 				if renameFile {
 					uploadedFile.NewFileName = fmt.Sprintf("%s%s", t.RandomString(25), filepath.Ext(hdr.Filename))
 				} else {
-			        uploadedFile.NewFileName = hdr.Filename		
+					uploadedFile.NewFileName = hdr.Filename
 				}
 
-                uploadedFile.OriginalFileName = hdr.Filename
-                var outfile *os.File
-                defer outfile.Close()
+				uploadedFile.OriginalFileName = hdr.Filename
+				var outfile *os.File
+				defer outfile.Close()
 
-                if outfile, err = os.Create(filepath.Join(uploadDir, uploadedFile.NewFileName)); err != nil {
-                    return nil, err
-                } else {
-                    fileSize, err := io.Copy(outfile, infile)
-                    if err != nil {
-                        return nil, err
-                    }
-                    uploadedFile.FileSize = fileSize
-                }
-            
-                uploadedFiles = append(uploadedFiles, &uploadedFile)
+				if outfile, err = os.Create(filepath.Join(uploadDir, uploadedFile.NewFileName)); err != nil {
+					return nil, err
+				} else {
+					fileSize, err := io.Copy(outfile, infile)
+					if err != nil {
+						return nil, err
+					}
+					uploadedFile.FileSize = fileSize
+				}
 
-                return uploadedFiles, nil
+				uploadedFiles = append(uploadedFiles, &uploadedFile)
+
+				return uploadedFiles, nil
 			}(uploadedFiles)
-            if err != nil {
-                return uploadedFiles, err
-            }
+			if err != nil {
+				return uploadedFiles, err
+			}
 		}
 	}
 
@@ -152,38 +157,136 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 // CreateDirIfNotExists creates a directory and all necessary parents
 // if it does not exists
 func (t *Tools) CreateDirIfNotExists(path string) error {
-    const mode = 0755
-    if _, err := os.Stat(path); os.IsNotExist(err) {
-        err := os.MkdirAll(path, mode)
-        if err != nil {
-            return err
-        }
-    }
-    return nil
+	const mode = 0755
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.MkdirAll(path, mode)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // Slugify is a very simple mean of creating a slug
 // from a string
 func (t *Tools) Slugify(s string) (string, error) {
-    if s == "" {
-        return "", errors.New("empty string not permitted")
-    }
+	if s == "" {
+		return "", errors.New("empty string not permitted")
+	}
 
-    var re = regexp.MustCompile(`[^a-z\d]+`)
-    slug := strings.Trim(re.ReplaceAllString(strings.ToLower(s), "-"), "-")
-    if len(slug) == 0 {
-        return "", errors.New("after removing characters slug is zero length")
-    }
+	var re = regexp.MustCompile(`[^a-z\d]+`)
+	slug := strings.Trim(re.ReplaceAllString(strings.ToLower(s), "-"), "-")
+	if len(slug) == 0 {
+		return "", errors.New("after removing characters slug is zero length")
+	}
 
-    return slug, nil
+	return slug, nil
 }
 
 // DownloadStaticFile downloads a file
 // and avoid the browser from displaying and forces download
 // by setting content-disposition
 func (t *Tools) DownloadStaticFile(w http.ResponseWriter, r *http.Request, p string, file string, displayName string) {
-    fp := path.Join(p, file)
-    w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", displayName))
+	fp := path.Join(p, file)
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", displayName))
 
-    http.ServeFile(w, r, fp)
+	http.ServeFile(w, r, fp)
+}
+
+// JSONResponse is the type used for sending JSON around
+type JSONResponse struct {
+	Error   bool        `json:"error"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+// ReadJSON tries to read the body of a request and converts it from json into a Go data variable
+func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1024 * 1024 //one meq
+	if t.MaxJSONSize != 0 {
+		maxBytes = t.MaxJSONSize
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+
+	if !t.AllowUnknownFields {
+		dec.DisallowUnknownFields()
+	}
+
+	err := dec.Decode(data)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly formed JSON (at character: %d)", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly formed JSON")
+		case errors.As(err, &unmarshalTypeError):
+            if unmarshalTypeError.Field != "" {
+                return fmt.Errorf("body contains incorrect JSON type for field: %q", unmarshalTypeError.Field)
+            }
+            return fmt.Errorf("body contains incorrect JSON type (at character: %d)", unmarshalTypeError.Offset)
+        case errors.Is(err, io.EOF):
+            return errors.New("body must not be empty")
+        case strings.HasPrefix(err.Error(), "json: unknown field"):
+            fieldName:= strings.TrimPrefix(err.Error(), "json: unknown field")
+            return fmt.Errorf("body contains unknown key: %s", fieldName)
+        case err.Error() == "http: request body too large":
+            return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+        case errors.As(err, &invalidUnmarshalError):
+            return fmt.Errorf("error unmarshalling JSON: %s", err.Error())
+		default:
+			return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must contain only one JSON value")
+	}
+
+	return nil
+}
+
+// WriteJSON takes a response status cdoe and a artbitrary data and write json to the client
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+    out, err := json.Marshal(data)
+    if err != nil {
+        return err
+    }
+
+    if len(headers) > 0 {
+        for key, value := range headers[0] {
+            w.Header()[key] = value
+        }
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(status)
+    _, err = w.Write(out)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+// ErrorJSON takes an error and optionally a statuscode and generates and sends a JSON error message
+func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+    statusCode := http.StatusBadRequest
+
+    if len(status) > 0 {
+        statusCode = status[0]
+    }
+
+    var payload JSONResponse
+    payload.Error = true
+    payload.Message = err.Error()
+
+    return t.WriteJSON(w, statusCode, payload)
 }
